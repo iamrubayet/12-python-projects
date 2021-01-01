dd4415759a7b9fe75aa9b09c0801777fdd8a98c1
rubayet170746 <rafirubayet75@gmail.com>
2021-01-01 17:40:05 +0600

    some changes

--- rock_paper.py
+++ rock_paper.py
@@ -2,7 +2,7 @@ import random
 
 
 def play():
-	user = input(" 'r' for rock,'p' for paper , 's' for scissors")
+	user = input(" what is your choice?? 'r' for rock,'p' for paper , 's' for scissors ")
 	computer = random.choice(['r','p','s'])
 
 
@@ -21,6 +21,8 @@ def play():
 def is_win(player,opponent):
 	if( player== 'r' and opponent == 's') or ( player == 's' and opponent == 'p') or ( player == 'p' and opponent =='r'):
 		return true
+
+print (play)
 
 
 
